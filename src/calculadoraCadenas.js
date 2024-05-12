dcc54275bfef18ed072455dc909aaead3bac98a1
Arturo <arturo.veliz@ucb.edu.bo>
2024-05-12 19:39:51 -0400

    Se añadio funcionalidad de devolver el mismo numero que se envia en la cadena a metodo calculadoraCadena

--- src/calculadoraCadenas.js
+++ src/calculadoraCadenas.js
@@ -1,8 +1,13 @@
 function calculadoraCadena(cadena) {
     let sumaDeNumeros;
+    let numero=parseInt(cadena);
     if(cadena=="")
     {
         sumaDeNumeros=0;
+    }
+    else
+    {
+        sumaDeNumeros=numero;
     }
     return sumaDeNumeros;
   }
--- src/calculadoraCadenas.spec.js
+++ src/calculadoraCadenas.spec.js
@@ -4,5 +4,7 @@ describe("Calcular numeros de una cadena", () => {
   it("deberia de devolver 0 para cadena vacia", () => {
     expect(calculadoraCadena("")).toEqual(0);
   });
-
+  it("deberia de devolver 2 para cadena con 2", () => {
+    expect(calculadoraCadena("2")).toEqual(2);
+  });
 });
