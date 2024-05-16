bfc4e24de4314aa2862f386c8b215b4dedce49cd
Arturo <arturo.veliz@ucb.edu.bo>
2024-05-16 01:30:22 -0400

    Se refactorizo codigo de presenter.js para nombres descriptivos de variables

--- src/presenter.js
+++ src/presenter.js
@@ -1,15 +1,15 @@
 import sumarNumerosDe from "./sumarNumerosDeCadena.js";
 
-const cadena = document.querySelector("#cadena");
-const form = document.querySelector("#calcular-form");
-const div = document.querySelector("#resultado-div");
+const Inputcadena = document.querySelector("#cadena");
+const formCalcular = document.querySelector("#calcular-form");
+const divResultado = document.querySelector("#resultado-div");
 
-form.addEventListener("submit", (event) => {
+formCalcular.addEventListener("submit", (event) => {
   event.preventDefault();
 
-  // No necesitas convertir la cadena a un entero, ya que es una cadena
-  const cadenaNumeros = cadena.value;
 
-  // Cambia String.parseInt por parseInt
-  div.innerHTML = "<p> suma de numeros: " + sumarNumerosDe(cadenaNumeros) + "</p>";
+  const cadena = Inputcadena.value;
+
+
+  divResultado.innerHTML = "<p> suma de numeros: " + sumarNumerosDe(cadena) + "</p>";
 });
